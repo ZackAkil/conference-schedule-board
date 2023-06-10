6305b3c0da27f535d02d63d9008bc437c8e8eb5e
Zack <zackakil94@gmail.com>
2023-06-10 18:51:55 +0100

    refactor

---
+++ data.js
@@ -0,0 +1,32 @@
+const profiles = {
+    'zack': { 'name': 'Zack Akil', 'profile_img': 'profiles/zack.webp' },
+    'polong': { 'name': 'Polong Lin', 'profile_img': 'profiles/polong.webp' },
+    'ivan': { 'name': 'Ivan Nardini', 'profile_img': 'profiles/ivan.webp' }
+}
+
+const events = [
+    // {
+    //     'profile': profiles['zack'], 'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who’s won in every hackathon they\'ve entered',
+    //     'start_time': '16:50', 'end_time': '17:15', 'status': 'now', 'location': 'Headliners Stage'
+    // },
+    {
+        'profile': profiles['zack'], 'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who’s won in every hackathon they\'ve entered',
+        'start_time': '16:50', 'end_time': '18:15', 'status': 'now', 'location': 'Headliners Stage', 'type': 'Keynote'
+    },
+    {
+        'profile': profiles['polong'], 'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
+        'start_time': '18:10', 'end_time': '18:50', 'status': 'soon', 'location': 'Next Generation Applications Stage', 'type': 'Panel'
+    },
+    // {
+    //     'profile': profiles['polong'], 'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
+    //     'start_time': '16:10', 'end_time': '16:50', 'status': 'soon', 'location': 'Next Generation Applications Stage'
+    // },
+    {
+        'profile': profiles['ivan'], 'event_name': 'How to Embrace This Wave of Innovation',
+        'start_time': '19:30', 'end_time': '19:25', 'status': 'later', 'location': 'AI at Scale Stage', 'type': 'Panel'
+    },
+    {
+        'profile': profiles['ivan'], 'event_name': 'Using AI to Reshape the Future of Retail',
+        'start_time': '18:35', 'end_time': '18:40', 'status': 'later', 'location': 'Industries Stage', 'type': 'Talk'
+    }
+]

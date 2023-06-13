bff8322a559c2a50b66abb86eca62efcbd12df22
Zack <zackakil94@gmail.com>
2023-06-13 23:30:51 +0100

    update data

--- data.js
+++ data.js
@@ -13,7 +13,7 @@ const events = [
         'profile': profiles['zack'],
         'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who\'s won in every hackathon they\'ve entered',
         'start_time': '16:50',
-        'end_time': '18:15',
+        'end_time': '17:15',
         'status': 'now',
         'location': 'Headliners Stage',
         'type': 'Keynote'
@@ -31,29 +31,29 @@ const events = [
     //     'profile': profiles['polong'], 'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
     //     'start_time': '16:10', 'end_time': '16:50', 'status': 'soon', 'location': 'Next Generation Applications Stage'
     // },
-    {
-        'profile': profiles['ivan'],
-        'event_name': 'How to Embrace This Wave of Innovation',
-        'start_time': '19:30',
-        'end_time': '19:25',
-        'status': 'later',
-        'location': 'AI at Scale Stage',
-        'type': 'Panel'
-    },
-    {
-        'profile': profiles['ivan'],
-        'event_name': 'Using AI to Reshape the Future of Retail',
-        'start_time': '18:35',
-        'end_time': '18:40',
-        'status': 'later',
-        'location': 'Industries Stage',
-        'type': 'Talk'
-    }
+    // {
+    //     'profile': profiles['ivan'],
+    //     'event_name': 'How to Embrace This Wave of Innovation',
+    //     'start_time': '19:30',
+    //     'end_time': '19:25',
+    //     'status': 'later',
+    //     'location': 'AI at Scale Stage',
+    //     'type': 'Panel'
+    // },
+    // {
+    //     'profile': profiles['ivan'],
+    //     'event_name': 'Using AI to Reshape the Future of Retail',
+    //     'start_time': '18:35',
+    //     'end_time': '18:40',
+    //     'status': 'later',
+    //     'location': 'Industries Stage',
+    //     'type': 'Talk'
+    // }
 ]
 
 const videoSources = [
-    'videos/Leading companies build with generative AI on Google Cloud.mp4',
-    'videos/Generate and edit images with Generative AI Studio.mp4',
-    'videos/Leading companies build with generative AI on Google Cloud.mp4',
-    'videos/Introduction to Vertex AI Model Garden.mp4'
+    'videos/Leading companies build with generative AI on Google Cloud [subs].mp4',
+    'videos/Generate and edit images with Generative AI Studio [subs].mp4',
+    'videos/Leading companies build with generative AI on Google Cloud [subs].mp4',
+    'videos/Introduction to Vertex AI Model Garden [subs].mp4'
 ];

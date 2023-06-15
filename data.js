35571dfe2e544d3c284573a6d3a88be2050424fd
Zack <zackakil94@gmail.com>
2023-06-15 07:23:28 +0100

    add new data

--- data.js
+++ data.js
@@ -1,7 +1,8 @@
 const profiles = {
     'zack': { 'name': 'Zack Akil', 'profile_img': 'profiles/zack.webp' },
     'polong': { 'name': 'Polong Lin', 'profile_img': 'profiles/polong.webp' },
-    'ivan': { 'name': 'Ivan Nardini', 'profile_img': 'profiles/ivan.webp' }
+    'ivan': { 'name': 'Ivan Nardini', 'profile_img': 'profiles/ivan.webp' },
+    'laurence': { 'name': 'Laurence Moroney', 'profile_img': 'profiles/lm.webp' }
 }
 
 const events = [
@@ -9,46 +10,73 @@ const events = [
     //     'profile': profiles['zack'], 'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who’s won in every hackathon they\'ve entered',
     //     'start_time': '16:50', 'end_time': '17:15', 'status': 'now', 'location': 'Headliners Stage'
     // },
-    {
-        'profile': profiles['zack'],
-        'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who\'s won in every hackathon they\'ve entered',
-        'start_time': '16:50',
-        'end_time': '17:15',
-        'status': 'now',
-        'location': 'Headliners Stage',
-        'type': 'Keynote'
-    },
-    {
-        'profile': profiles['polong'],
-        'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
-        'start_time': '16:10',
-        'end_time': '16:50',
-        'status': 'soon',
-        'location': 'Next Generation Applications Stage',
-        'type': 'Panel'
-    },
+    // {
+    //     'profile': profiles['zack'],
+    //     'event_name': 'How to Have Winning AI Ideas: Demos and frameworks from an ML engineer who\'s won in every hackathon they\'ve entered',
+    //     'start_time': '16:50',
+    //     'end_time': '17:15',
+    //     'status': 'now',
+    //     'location': 'Headliners Stage',
+    //     'type': 'Keynote'
+    // },
+    // {
+    //     'profile': profiles['polong'],
+    //     'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
+    //     'start_time': '16:10',
+    //     'end_time': '16:50',
+    //     'status': 'soon',
+    //     'location': 'Next Generation Applications Stage',
+    //     'type': 'Panel'
+    // },
     // {
     //     'profile': profiles['polong'], 'event_name': 'Types of Data Configured for Businesses: Can Implementing Them Change Your Growth Opportunity?',
     //     'start_time': '16:10', 'end_time': '16:50', 'status': 'soon', 'location': 'Next Generation Applications Stage'
     // },
-    // {
-    //     'profile': profiles['ivan'],
-    //     'event_name': 'How to Embrace This Wave of Innovation',
-    //     'start_time': '19:30',
-    //     'end_time': '19:25',
-    //     'status': 'later',
-    //     'location': 'AI at Scale Stage',
-    //     'type': 'Panel'
-    // },
-    // {
-    //     'profile': profiles['ivan'],
-    //     'event_name': 'Using AI to Reshape the Future of Retail',
-    //     'start_time': '18:35',
-    //     'end_time': '18:40',
-    //     'status': 'later',
-    //     'location': 'Industries Stage',
-    //     'type': 'Talk'
-    // }
+    {
+        'profile': profiles['ivan'],
+        'event_name': 'How to Embrace This Wave of Innovation',
+        'start_time': '10:00',
+        'end_time': '10:25',
+        'status': 'later',
+        'location': 'AI at Scale Stage',
+        'type': 'Talk'
+    },
+    {
+        'profile': profiles['ivan'],
+        'event_name': 'Using AI to Reshape the Future of Retail',
+        'start_time': '11:15',
+        'end_time': '11:40',
+        'status': 'later',
+        'location': 'Next Generation Applications',
+        'type': 'Panel'
+    },
+    {
+        'profile': profiles['zack'],
+        'event_name': 'Developing the Next Generation within AI',
+        'start_time': '12:10',
+        'end_time': '12:50',
+        'status': 'now',
+        'location': 'Diversity In AI Stage',
+        'type': 'Panel'
+    },
+    {
+        'profile': profiles['laurence'],
+        'event_name': 'Generative AI: Cutting Through the Hype',
+        'start_time': '11:00',
+        'end_time': '11:25',
+        'status': 'now',
+        'location': 'Practitioners Stage',
+        'type': 'Talk'
+    },
+    {
+        'profile': profiles['laurence'],
+        'event_name': 'Impulse vs Control: A reality check of what generative AI means for innovation, ethics and cybersecurity in the AI space - how do we regulate fast moving tech?',
+        'start_time': '09:45',
+        'end_time': '10:30',
+        'status': 'now',
+        'location': 'Headliners Stage',
+        'type': 'Fireside Chat'
+    },
 ]
 
 const videoSources = [
